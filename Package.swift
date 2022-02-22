e17fe99d8cd1d368c4f892f2fe3858726ff92b9f
Alex Chase <alexchase55@gmail.com>
2022-02-21 22:09:53 -0800

    Fix package

--- Package.swift
+++ Package.swift
@@ -13,18 +13,20 @@ let package = Package(
             targets: ["Sliders"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/kieranb662/bez.git", from: "1.0.0"),
         .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1"),
-        .package(url: "https://github.com/kieranb662/Shapes.git", from: "1.0.2"),
-        .package(url: "https://github.com/kieranb662/bez.git", from: "1.0.0")
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package( url: "https://github.com/kieranb662/SwiftUI-Shapes.git", from: "1.0.10"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Sliders",
-            dependencies: ["CGExtender", "Shapes", "bez"]),
+            dependencies: [
+                "bez",
+                "CGExtender",
+                .product(name: "Shapes", package: "SwiftUI-Shapes"),
+            ]),
         .testTarget(
             name: "SlidersTests",
             dependencies: ["Sliders"]),
